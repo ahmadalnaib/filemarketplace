9f4015ff32fc790162ff848cf554848d93125be8
ahmedhabeeb73 <alnaib888@gmail.com>
2023-08-05 17:20:35 +0200

    05. Automatically creating Stripe accounts for your users

--- app/Http/Controllers/StripeOnboardingController.php
+++ app/Http/Controllers/StripeOnboardingController.php
@@ -8,6 +8,11 @@ class StripeOnboardingController extends Controller
 {
     //
 
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function index()
     {
         return view('onboarding');
