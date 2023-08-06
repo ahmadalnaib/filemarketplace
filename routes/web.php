30bba9fe76d1adebe48fcbd5e54e8f41c77c5d9b
ahmedhabeeb73 <alnaib888@gmail.com>
2023-08-06 11:13:32 +0200

    08. Setting up and listing products

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductIndexController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StripeOnboardingController;
 use Illuminate\Support\Facades\Route;
@@ -23,7 +24,7 @@ Route::get('/onboarding/redirect',[StripeOnboardingController::class,'redirect']
 Route::get('/onboarding/verity',[StripeOnboardingController::class,'verity'])->name('onboarding.verity');
 
 Route::get('/dashboard',DashboardController::class)->name('dashboard');
-
+Route::get('/products',ProductIndexController::class)->name('products');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
